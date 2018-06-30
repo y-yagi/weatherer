659845ed7b6d91188268d7cb87731a0c33d01208
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-06-30 13:02:08 +0900

    First commit

---
+++ cmd/weatherer/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/y-yagi/configure"
+	"github.com/y-yagi/weatherer"
+)
+
+const cmd = "weatherer"
+
+type config struct {
+	DataBase string `toml:"database"`
+}
+
+var cfg config
+
+func init() {
+	err := configure.Load(cmd, &cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(cfg.DataBase) == 0 {
+		cfg.DataBase = filepath.Join(configure.ConfigDir(cmd), cmd+".db")
+		configure.Save(cmd, cfg)
+	}
+}
+
+func main() {
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
+}
+
+func run(args []string, outStream, errStream io.Writer) (exitCode int) {
+	var importFile string
+	var config bool
+
+	exitCode = 0
+
+	flags := flag.NewFlagSet(cmd, flag.ExitOnError)
+	flags.SetOutput(errStream)
+	flags.StringVar(&importFile, "i", "", "Import file.")
+	flags.BoolVar(&config, "c", false, "Edit config.")
+	flags.Parse(args[1:])
+
+	if config {
+		editor := os.Getenv("EDITOR")
+		if len(editor) == 0 {
+			editor = "vim"
+		}
+
+		if err := configure.Edit(cmd, editor); err != nil {
+			fmt.Fprintf(errStream, "Error: %v\n", err)
+			exitCode = 1
+			return
+		}
+		return
+	}
+
+	we := weatherer.NewWeatherer(cfg.DataBase)
+	err := we.InitDB()
+	if err != nil {
+		fmt.Fprintf(errStream, "Error: %v\n", err)
+		exitCode = 1
+		return
+	}
+
+	if len(importFile) != 0 {
+		if err = we.Import(importFile); err != nil {
+			fmt.Fprintf(errStream, "Error: %v\n", err)
+			exitCode = 1
+			return
+		}
+		return
+	}
+
+	cmd := exec.Command("sqlite3", cfg.DataBase)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err = cmd.Run(); err != nil {
+		fmt.Fprintf(errStream, "Error: %v\n", err)
+		exitCode = 1
+		return
+	}
+	return
+}
---
+++ weatherer.go
@@ -0,0 +1,119 @@
+package weatherer
+
+import (
+	"encoding/csv"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/y-yagi/goext/osext"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+var (
+	schema = `
+CREATE TABLE weathers (
+	id integer primary key autoincrement not null,
+	area varchar not null,
+	date date not null,
+	hour integer not null,
+	temperature float not null,
+	precipitation float,
+	wind_speed float,
+	wind_direction varchar,
+	created_at datetime not null,
+	unique(area, date)
+);
+`
+	insertQuery = `
+INSERT INTO weathers
+	(area , date, hour, temperature, precipitation, wind_speed, wind_direction, created_at)
+	VALUES
+	($1, $2, $3, $4, $5, $6, $7, $8)
+`
+)
+
+type Weatherer struct {
+	database string
+}
+
+type Weather struct {
+	ID            int       `db:"id"`
+	Area          string    `db:"area"`
+	Date          time.Time `db:"date"`
+	hour          int       `db:"hour"`
+	temperature   float32   `db:"temperature"`
+	windSpeed     float32   `db:"wind_speed"`
+	windDirection string    `db:"wind_direction"`
+	CreatedAt     time.Time `db:"created_at"`
+}
+
+// NewWeatherer creates a new weatherer.
+func NewWeatherer(database string) *Weatherer {
+	weatherer := &Weatherer{database: database}
+	return weatherer
+}
+
+// InitDB initialize database.
+func (w *Weatherer) InitDB() error {
+	if osext.IsExist(w.database) {
+		return nil
+	}
+
+	db, err := sqlx.Connect("sqlite3", w.database)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	db.MustExec(schema)
+
+	return nil
+}
+
+// Import file to database
+func (w *Weatherer) Import(filename string) error {
+	headerLines := 5
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	r := csv.NewReader(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
+	r.FieldsPerRecord = -1
+	if err != nil {
+		return err
+	}
+
+	records, err := r.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	db, err := sqlx.Connect("sqlite3", w.database)
+	if err != nil {
+		return err
+	}
+
+	format := "2006/1/2 15:04:05"
+	_, filename = filepath.Split(filename)
+	area := strings.Split(filename, "_")[0]
+
+	tx := db.MustBegin()
+	for _, record := range records[headerLines:] {
+		t, err := time.Parse(format, record[0])
+		if err != nil {
+			return err
+		}
+		tx.MustExec(insertQuery, area, t, t.Hour(), record[1], record[2], record[4], record[7], record[9], time.Now())
+	}
+
+	tx.Commit()
+	return nil
+}
